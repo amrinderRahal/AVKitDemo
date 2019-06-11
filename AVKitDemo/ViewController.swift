a159a896be74ffbea65d7b7150c7c40a7a957c23
Amrinder Singh <AmrinderRahal>
2019-06-11 14:11:14 +0530

    Initial Commit

---
+++ AVKitDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AVKitDemo
+//
+//  Created by Amandeep Singh on 11/06/19.
+//  Copyright © 2019 Amandeep Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
